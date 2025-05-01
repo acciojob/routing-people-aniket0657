7f51be119330f8295bfc90a664a9c67c48d1543d
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-01 16:09:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData] = useState(null)
+  const [isLoading,setIsLoading] = useState(true)
+  const [error,setError] = useState('')
+
+  const getData =  ()=>{
+    
+     fetch('https://dummyjson.com/products').then((res)=>res.json()).then((resData)=>{
+      setData(resData)
+}).catch((e)=>{
+      const message = e.message
+       setError(message)
+     }).finally(()=>{
+  setIsLoading(false)
+})
+  }
+  useEffect(()=>{
+      getData()
+},[])
   return (
     <div>
-        {/* Do not remove the main div */}
+    {isLoading && <p>Loading...</p>}
+  {!!error && <p>An error occurred: {error}</p>}
+  {!!data&&
+    <>
+       <h1>Data Fetched from API</h1>
+   <pre>
+       {JSON.stringify(data,null,2)}
+  </pre>
+    </>
+  }
+  {
+  !data && !isLoading &&!error&& <p>No data found</p>
+}
     </div>
   )
 }
