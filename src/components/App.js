9c5a50abbda31d015f9b37397009dfd941dc7c97
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-05-08 14:58:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
-
-const App = () => {
-  const [data,setData] = useState(null)
-  const [isLoading,setIsLoading] = useState(true)
-  const [error,setError] = useState('')
-
-  const getData =  ()=>{
-    
-     fetch('https://dummyjson.com/products').then((res)=>res.json()).then((resData)=>{
-      setData(resData)
-}).catch((e)=>{
-      const message = e.message
-       setError(message)
-     }).finally(()=>{
-  setIsLoading(false)
-})
-  }
-  useEffect(()=>{
-      getData()
-},[])
+function App() {
   return (
-    <div>
-    {isLoading && <p>Loading...</p>}
-  {!!error && <p>An error occurred: {error}</p>}
-  {!!data&&
-    <>
-       <h1>Data Fetched from API</h1>
-   <pre>
-       {JSON.stringify(data,null,2)}
-  </pre>
-    </>
-  }
-  {
-  !data && !isLoading &&!error&& <p>No data found</p>
-}
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
